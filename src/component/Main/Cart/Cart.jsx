4f0c4800954ee8ad5e8a91602da25df23d47ba05
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-04-28 19:34:41 +0800

    feat: Add increase/decrease shopping list buttons in Cart component

--- src/component/Main/Cart/Cart.jsx
+++ src/component/Main/Cart/Cart.jsx
@@ -1,54 +1,94 @@
 import styles from './Cart.module.scss'
-import product01 from 'assets/images/product-1.jpg'
-import product02 from 'assets/images/product-2.jpg'
+import { useState } from 'react'
 //  ---------------------------------------------------------
 const cartData = [
   {
     id: '1',
-    name: '破壞補丁修身牛仔褲',
-    img: product01,
+    name: '貓咪罐罐',
+    img: 'https://picsum.photos/300/300?text=1',
     price: 100,
-    quantity: 1
+    quantity: 2
   },
   {
     id: '2',
-    name: '刷色直筒牛仔褲',
-    img: product02,
+    name: '貓咪干干',
+    img: 'https://picsum.photos/300/300?text=2',
     price: 200,
     quantity: 1
   }
 ]
-const ProductListItem = ({ icons, item }) => {
+const ProductListItem = ({ icons, item, onCartItemsChange }) => {
   return (
     <div className={`${styles.productContainer} col col-12`} data-count='0' data-price={item.price}>
-      <img className={styles.imgContainer} src={item.img} />
+      <img className={styles.imgContainer} src={item.img} alt={item.name} />
       <div className={styles.productInfo}>
         <div className={styles.productName}>{item.name}</div>
         <div className={styles.productControlContainer}>
           <div className={styles.productControl}>
-            <svg className={`${styles.productAction} minus`}>
-              <use xlinkHref={`${icons}#svg-icon-minus`} />
-            </svg>
+            <span
+              onClick={() => {
+                onCartItemsChange?.({ id: item.id, quantity: item.quantity - 1 })
+              }}
+            >
+              <svg className={`${styles.productAction} minus`}>
+                <use xlinkHref={`${icons}#svg-icon-minus`} />
+              </svg>
+            </span>
             <span className={styles.productCount}>{item.quantity}</span>
-            <svg className={`${styles.productAction} plus`}>
-              <use xlinkHref={`${icons}#svg-icon-plus`} />
-            </svg>
+            <span
+              onClick={() => {
+                onCartItemsChange?.({ id: item.id, quantity: item.quantity + 1 })
+              }}
+            >
+              <svg className={`${styles.productAction} plus`}>
+                <use xlinkHref={`${icons}#svg-icon-plus`} />
+              </svg>
+            </span>
           </div>
         </div>
-        <div className={styles.price}>{item.price}</div>
+        <div className={styles.price}>{item.price * item.quantity}</div>
       </div>
     </div>
   )
 }
 
 const Cart = ({ icons }) => {
+  const [items, setItems] = useState(cartData)
+
+  const handleCartItemsChange = ({ id, quantity }) => {
+    if (quantity < 0) {
+      return
+    }
+    setItems((prevItems) => {
+      return prevItems.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            quantity
+          }
+        }
+        return item
+      })
+    })
+  }
+
+  // 計算加總金額
+  let count = 0
+  items.forEach(item => {
+    count = count + item.price * item.quantity
+  })
   return (
     <>
       <section className={`${styles.cartContainer} col col-lg-5 col-sm-12`}>
         <h3 className={styles.cartTitle}>購物籃</h3>
         <section className={`${styles.productList} col col-12`} data-total-price='0'>
-          {cartData.map((item) => (
-            <ProductListItem item={item} icons={icons} key={item.id} />
+          {items.map((item) => (
+            <ProductListItem
+              item={item}
+              icons={icons}
+              key={item.id}
+              onCartItemsChange={handleCartItemsChange}
+            />
           ))}
         </section>
         {/* --------- 結帳  -------- */}
@@ -58,7 +98,7 @@ const Cart = ({ icons }) => {
         </section>
         <section className={`${styles.cartInfo} ${styles.total} col col-12`}>
           <div className={styles.text}>小計</div>
-          <div className={styles.price}>$300</div>
+          <div className={styles.price}>${count}</div>
         </section>
       </section>
     </>
