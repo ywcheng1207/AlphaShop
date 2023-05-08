a36cf8a9c9c66db2d2f45dbbf738384d151701df
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-05-08 22:21:03 +0800

    feat: Add form switch button functionality

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -4,15 +4,30 @@ import './Main.scss'
 import Register from './Register/Register'
 import Cart from './Cart/Cart'
 import ProgressControl from './ProgressControl/ProgressControl'
-
+// --
+import { useState } from 'react'
+// --
 const Main = ({ icons }) => {
+  const [step, setStep] = useState(0)
+  const [shippingCost, setShippingCost] = useState('免費')
+
+  const handleStepItemChange = ({ step }) => setStep(step)
+  const handleShippingOption = ({ price }) => setShippingCost(price)
   return (
     <>
       <main className='site-main'>
         <div className='main-container'>
-          <Register icons={icons} />
-          <Cart icons={icons} />
-          <ProgressControl icons={icons} />
+          <Register
+            icons={icons}
+            step={step}
+            onShippingOption={handleShippingOption}
+          />
+          <Cart icons={icons} shippingCost={shippingCost} />
+          <ProgressControl
+            icons={icons}
+            step={step}
+            onStepItemChange={handleStepItemChange}
+          />
         </div>
       </main>
     </>
--- src/component/Main/ProgressControl/ProgressControl.jsx
+++ src/component/Main/ProgressControl/ProgressControl.jsx
@@ -1,40 +1,70 @@
 import styles from './ProgressControl.module.scss'
 
-const ProgressControl = ({ icons }) => {
+const StepItem = ({ icons, step, onStepItemChange }) => {
+  if (step === 0) {
+    return (
+      <section className={`${styles.buttonGroup} col col-12`} dataphase='address'>
+        <button
+          className={`${styles.next} cursor-point`}
+          onClick={() => { onStepItemChange?.({ step: step + 1 }) }}
+        >
+          下一步
+          <svg>
+            <use xlinkHref={`${icons}#svg-icon-right-arrow`} />
+          </svg>
+        </button>
+      </section>
+    )
+  } else if (step === 1) {
+    return (
+      <section className={`${styles.buttonGroup} col col-12`}>
+        <button
+          className={`${styles.prev} cursor-point`}
+          onClick={() => { onStepItemChange?.({ step: step - 1 }) }}
+        >
+          <svg>
+            <use xlinkHref={`${icons}#svg-icon-left-arrow`} />
+          </svg>
+          上一步
+        </button>
+        <button
+          className={`${styles.next} cursor-point`}
+          onClick={() => { onStepItemChange?.({ step: step + 1 }) }}
+        >
+          下一步
+          <svg>
+            <use xlinkHref={`${icons}#svg-icon-right-arrow`} />
+          </svg>
+        </button>
+      </section>
+    )
+  } else if (step === 2) {
+    return (
+      <section className={`${styles.buttonGroup} col col-12`}>
+        <button
+          className={`${styles.prev} cursor-point`}
+          onClick={() => { onStepItemChange?.({ step: step - 1 }) }}
+        >
+          <svg>
+            <use xlinkHref={`${icons}#svg-icon-left-arrow`} />
+          </svg>
+          上一步
+        </button>
+        <button className={`${styles.next} cursor-point`}>確認下單</button>
+      </section>
+    )
+  }
+}
+
+const ProgressControl = ({ icons, step, onStepItemChange }) => {
   return (
     <>
       <section className={`${styles.progressControlContainer} col col-lg-6 col-sm-12`}>
-        <section className={`${styles.buttonGroup} col col-12`} data-phase='address'>
-          <button className={`${styles.next} cursor-point`}>
-            下一步
-            <svg>
-              <use xlinkHref={`${icons}#svg-icon-right-arrow`} />
-            </svg>
-          </button>
-        </section>
-        {/* <section className={`${styles.buttonGroup} col col-12`} dataphase='shipping'>
-          <button className={`${styles.prev} cursor-point`}>
-            <svg>
-              <use xlinkHref={`${icons}#svg-icon-left-arrow`} />
-            </svg>
-            上一步
-          </button>
-          <button className={`${styles.next} cursor-point`}>
-            下一步
-            <svg>
-              <use xlinkHref={`${icons}#svg-icon-right-arrow`} />
-            </svg>
-          </button>
-        </section> */}
-        {/* <section className={`${styles.buttonGroup} col col-12`} dataphase='credit-card'>
-          <button className={`${styles.prev} cursor-point`}>
-            <svg>
-              <use xlinkHref={`${icons}#svg-icon-left-arrow`} />
-            </svg>
-            上一步
-          </button>
-          <button className={`${styles.next} cursor-point`}>確認下單</button>
-        </section> */}
+        <StepItem
+          icons={icons}
+          step={step}
+          onStepItemChange={onStepItemChange}
+        />
       </section>
     </>
   )
