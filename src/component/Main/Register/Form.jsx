c46996f1e9e99a35f5f25ca39ed9c5a7eb6889d6
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-05-08 22:24:55 +0800

    feat: Add form switch functionality

--- src/component/Main/Register/Form.jsx
+++ src/component/Main/Register/Form.jsx
@@ -1,12 +1,19 @@
 import { AddressPhase, CreditPhase, ShippingPhase } from './FormPhase'
 import styles from './Form.module.scss'
-const Form = () => {
+
+const Form = ({ step, onShippingOption }) => {
   return (
     <>
-      <section className={`${styles.formContainer} col col-12`}>
-        <AddressPhase />
-        {/* <ShippingPhase /> */}
-        {/* <CreditPhase /> */}
+      <section className={`${styles.formContainer}`}>
+        <div className={styles.address} dataphase={step}>
+          <AddressPhase />
+        </div>
+        <div className={styles.shipping} dataphase={step}>
+          <ShippingPhase onShippingOption={onShippingOption} />
+        </div>
+        <div className={styles.creddit} dataphase={step}>
+          <CreditPhase />
+        </div>
       </section>
     </>
   )
--- src/component/Main/Register/Register.jsx
+++ src/component/Main/Register/Register.jsx
@@ -1,13 +1,16 @@
 import Progress from './Progress'
 import Form from './Form'
 import styles from './Register.module.scss'
-const Register = ({ icons }) => {
+const Register = ({ icons, step, onShippingOption }) => {
   return (
     <>
       <section className={`${styles.registerContainer} col col-lg-6 col-sm-12`}>
         <h2 className={`${styles.registerTitle} col col-12`}>結帳</h2>
-        <Progress icons={icons} />
-        <Form />
+        <Progress icons={icons} step={step} />
+        <Form
+          step={step}
+          onShippingOption={onShippingOption}
+        />
       </section>
     </>
   )
